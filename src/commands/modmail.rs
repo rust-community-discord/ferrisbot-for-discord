800e1b8d06f180db492efb8691cfd2f42f1c5164
Ivan Dardi <ivy.dardi@gmail.com>
2023-09-05 19:23:45 -0300

    feat: add right click context menu for opening modmails

--- src/commands/modmail.rs
+++ src/commands/modmail.rs
@@ -4,6 +4,86 @@ use poise::serenity_prelude as serenity;
 use poise::serenity_prelude::Mentionable;
 use tracing::{debug, info};
 
+/// Opens a modmail thread for a message. To use, right click the message that
+/// you want to report, then go to "Apps" > "Open Modmail".
+#[poise::command(
+	ephemeral,
+	context_menu_command = "Open Modmail",
+	hide_in_help,
+	category = "Modmail"
+)]
+pub async fn modmail_context_menu_for_message(
+	ctx: Context<'_>,
+	#[description = "Message to automatically link when opening a modmail"]
+	message: serenity::Message,
+) -> Result<(), Error> {
+	let message = format!(
+		"Message reported: {}\n\nMessage contents:\n\n{}",
+		message.link_ensured(ctx).await,
+		message.content_safe(ctx)
+	);
+	create_modmail_thread(ctx, message).await?;
+	Ok(())
+}
+
+/// Opens a modmail thread for a guild member. To use, right click the member
+/// that you want to report, then go to "Apps" > "Open Modmail".
+#[poise::command(
+	ephemeral,
+	context_menu_command = "Open Modmail",
+	hide_in_help,
+	category = "Modmail"
+)]
+pub async fn modmail_context_menu_for_user(
+	ctx: Context<'_>,
+	#[description = "User to automatically link when opening a modmail"] user: serenity::User,
+) -> Result<(), Error> {
+	let message = format!("User reported:\n{}\n{}\n\nPlease provide additional information about the user being reported.", user.id, user.name);
+	create_modmail_thread(ctx, message).await?;
+	Ok(())
+}
+
+/// Send a private message to the moderators of the server.
+///
+/// Call this command in a channel when someone might be breaking the rules, for example by being \
+/// very rude, or starting discussions about divisive topics like politics and religion. Nobody \
+/// will see that you invoked this command.
+///
+/// You can also use this command whenever you want to ask private questions to the moderator team,
+/// open ban appeals, and generally anything that you need help with.
+///
+/// Your message, along with a link to the channel and its most recent message, will show up in a
+/// dedicated modmail channel for moderators, and it allows them to deal with it much faster than if
+/// you were to DM a potentially AFK moderator.
+///
+/// You can still always ping the Moderator role if you're comfortable doing so.
+#[poise::command(prefix_command, slash_command, ephemeral, category = "Modmail")]
+pub async fn modmail(
+	ctx: Context<'_>,
+	#[description = "What would you like to say?"] user_message: String,
+) -> Result<(), Error> {
+	let message = format!(
+		"{}\n\nSent from {}",
+		user_message,
+		ctx.channel_id().mention()
+	);
+	create_modmail_thread(ctx, message).await?;
+	Ok(())
+}
+
+#[poise::command(
+	prefix_command,
+	slash_command,
+	ephemeral,
+	category = "Modmail",
+	hide_in_help,
+	check = "crate::checks::check_is_moderator"
+)]
+pub async fn modmail_setup(ctx: Context<'_>) -> Result<(), Error> {
+	load_or_create_modmail_message(ctx, ctx.data()).await?;
+	Ok(())
+}
+
 pub async fn load_or_create_modmail_message(
 	http: impl AsRef<serenity::Http>,
 	data: &Data,
@@ -66,24 +146,16 @@ The modmail will materialize itself as a private thread under this channel with 
 	Ok(())
 }
 
-/// Send a private message to the moderators of the server.
-///
-/// Call this command in a channel when someone might be breaking the rules, for example by being \
-/// very rude, or starting discussions about divisive topics like politics and religion. Nobody \
-/// will see that you invoked this command.
-///
-/// You can also use this command whenever you want to ask private questions to the moderator team,
-/// open ban appeals, and generally anything that you need help with.
-///
-/// Your message, along with a link to the channel and its most recent message, will show up in a
-/// dedicated modmail channel for moderators, and it allows them to deal with it much faster than if
-/// you were to DM a potentially AFK moderator.
-///
-/// You can still always ping the Moderator role if you're comfortable doing so.
-#[poise::command(prefix_command, slash_command, ephemeral, category = "Modmail")]
-pub async fn modmail(
+/// It's important to keep this in a function because we're dealing with lifetimes and guard drops.
+async fn store_message(data: &Data, message: serenity::Message) {
+	info!("Storing modlog message on cache.");
+	let mut rwguard = data.modmail_message.write().await;
+	rwguard.get_or_insert(message);
+}
+
+async fn create_modmail_thread(
 	ctx: Context<'_>,
-	#[description = "What would you like to say?"] user_message: String,
+	user_message: impl Into<String>,
 ) -> Result<(), Error> {
 	load_or_create_modmail_message(ctx, ctx.data()).await?;
 
@@ -108,12 +180,10 @@ pub async fn modmail(
 		.await?;
 
 	let thread_message_content = format!(
-		"Hey <@&{}>, <@{}> needs help in channel {}: {}\n> {}",
+		"Hey <@&{}>, <@{}> needs help with the following:\n> {}",
 		ctx.data().mod_role_id,
 		ctx.author().id,
-		ctx.channel_id().mention(),
-		latest_message_link(ctx).await,
-		user_message
+		user_message.into()
 	);
 
 	modmail_thread
@@ -129,44 +199,10 @@ pub async fn modmail(
 		.await?;
 
 	ctx.say(format!(
-		"Successfully sent your message to the moderators. Check out the responses here: {}",
+		"Successfully sent your message to the moderators. Check out your modmail thread here: {}",
 		modmail_thread.mention()
 	))
 	.await?;
 
 	Ok(())
-}
-
-#[poise::command(
-	prefix_command,
-	slash_command,
-	ephemeral,
-	category = "Modmail",
-	hide_in_help,
-	check = "crate::checks::check_is_moderator"
-)]
-pub async fn modmail_setup(ctx: Context<'_>) -> Result<(), Error> {
-	load_or_create_modmail_message(ctx, ctx.data()).await?;
-	Ok(())
-}
-
-async fn latest_message_link(ctx: Context<'_>) -> String {
-	let message = ctx
-		.channel_id()
-		.messages(ctx, |get_messages| get_messages.limit(1))
-		.await
-		.ok()
-		.and_then(|messages| messages.into_iter().next());
-
-	match message {
-		Some(msg) => msg.link_ensured(ctx).await,
-		None => "<couldn't retrieve latest message link>".into(),
-	}
-}
-
-/// It's important to keep this in a function because we're dealing with lifetimes and guard drops.
-async fn store_message(data: &Data, message: serenity::Message) {
-	info!("Storing modlog message on cache.");
-	let mut rwguard = data.modmail_message.write().await;
-	rwguard.get_or_insert(message);
 }
