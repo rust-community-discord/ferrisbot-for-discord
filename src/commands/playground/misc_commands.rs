rust-community-discord/ferrisbot-for-discord

commit 7455f78ac2712dd6e8442fa63359101a74c04a95

Revert the formatting part of the syn wrap change (#30)
I did not test this locally. I attempted to revert it as cleanly as
possible.
If you prefer I can also revert the *entire* change which has a higher
chance of being correct.

fixes #29

---------

Co-authored-by: bendn <bend.n@outlook.com>

src/commands/playground/util.rs
@@ -210,6 +210,7 @@ pub fn maybe_wrapped(
 	use quote::quote;
 	use syn::{parse::Parse, *};
 
+	// We use syn to check whether there is a main function.
 	struct Inline {
 		attrs: Vec<Attribute>,
 		stmts: Vec<Stmt>,
@@ -230,36 +231,42 @@ pub fn maybe_wrapped(
 		}
 	}
 
-	let Ok(Inline { attrs, mut stmts }) = parse_str::<Inline>(code) else {
+	let Ok(Inline { .. }) = parse_str::<Inline>(code) else {
 		return Cow::Borrowed(code);
 	};
+
+	// These string subsitutions are not quite optimal, but they perfectly preserve formatting, which is very important.
+	// This function must not change the formatting of the supplied code or it will be confusing and hard to use.
+
+	// fn main boilerplate
+	let mut after_crate_attrs = match result_handling {
+		ResultHandling::None => "fn main() {\n",
+		ResultHandling::Discard => "fn main() { let _ = {\n",
+		ResultHandling::Print => "fn main() { println!(\"{:?}\", {\n",
+	}
+	.to_owned();
+
 	if unsf {
-		stmts = vec![Stmt::Expr(
-			Expr::Unsafe(ExprUnsafe {
-				attrs: vec![],
-				unsafe_token: syn::token::Unsafe::default(),
-				block: Block {
-					brace_token: syn::token::Brace::default(),
-					stmts,
-				},
-			}),
-			None,
-		)];
+		after_crate_attrs = format!("{after_crate_attrs}unsafe {{");
 	}
-	match result_handling {
-		ResultHandling::None => quote! { #(#attrs)* fn main() { #(#stmts)* } },
-		ResultHandling::Discard => {
-			quote! { #(#attrs)* fn main() { _ = (|| { #(#stmts)* })() } }
-		}
-		ResultHandling::Print if pretty => {
-			quote! { #(#attrs)* fn main() { ::std::println!("{:#?}", (|| { #(#stmts)* })()) } }
-		}
-		ResultHandling::Print => {
-			quote! { #(#attrs)* fn main() { ::std::println!("{:?}", (|| { #(#stmts)* })()) } }
-		}
+
+	// fn main boilerplate counterpart
+	let mut after_code = match result_handling {
+		ResultHandling::None => "}",
+		ResultHandling::Discard => "}; }",
+		ResultHandling::Print => "}); }",
 	}
-	.to_string()
-	.into()
+	.to_owned();
+
+	if unsf {
+		after_code = format!("}}{after_code}");
+	}
+
+	Cow::Owned(hoise_crate_attributes(
+		code,
+		&after_crate_attrs,
+		&after_code,
+	))
 }
 
 /// Send a Discord reply with the formatted contents of a Playground result

src/main.rs
@@ -86,8 +86,8 @@ async fn serenity(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> Shut
 					poise::Prefix::Literal("<:ferris:358652670585733120>"),
 					poise::Prefix::Literal("<:ferrisballSweat:678714352450142239> "),
 					poise::Prefix::Literal("<:ferrisballSweat:678714352450142239>"),
-					poise::Prefix::Literal("<:ferrisCat:678714352450142239> "),
-					poise::Prefix::Literal("<:ferrisCat:678714352450142239>"),
+					poise::Prefix::Literal("<:ferrisCat:1183779700485664820> "),
+					poise::Prefix::Literal("<:ferrisCat:1183779700485664820>"),
 					poise::Prefix::Literal("<:ferrisOwO:579331467000283136> "),
 					poise::Prefix::Literal("<:ferrisOwO:579331467000283136>"),
 					poise::Prefix::Regex(

commit 06c2b523f30d24f80944cae6ebce2deab237fb20

fixes the pretty printing that `:ferrisCat: eval` should cause (#43)
![image](https://github.com/user-attachments/assets/39c9a035-55ee-46e7-b661-e2c8b4bf21ba)


was broken by #30

src/commands/playground/misc_commands.rs
@@ -33,6 +33,7 @@ pub async fn miri(
 		&code.code,
 		ResultHandling::Discard,
 		ctx.prefix().contains("Sweat"),
+		false,
 	);
 	let (flags, flag_parse_errors) = parse_flags(flags);
 
@@ -160,6 +161,7 @@ pub async fn clippy(
 			&code.code,
 			ResultHandling::Discard,
 			ctx.prefix().contains("Sweat"),
+			false,
 		)
 	);
 	let (flags, flag_parse_errors) = parse_flags(flags);

src/commands/playground/play_eval.rs
@@ -20,7 +20,12 @@ async fn play_or_eval(
 ) -> Result<(), Error> {
 	ctx.say(stub_message(ctx)).await?;
 
-	let code = maybe_wrapped(&code.code, result_handling, ctx.prefix().contains("Sweat"));
+	let code = maybe_wrapped(
+		&code.code,
+		result_handling,
+		ctx.prefix().contains("Sweat"),
+		ctx.prefix().contains("OwO") || ctx.prefix().contains("Cat"),
+	);
 	let (mut flags, flag_parse_errors) = parse_flags(flags);
 
 	if force_warnings {

src/commands/playground/util.rs
@@ -200,11 +200,17 @@ pub fn hoise_crate_attributes(code: &str, after_crate_attrs: &str, after_code: &
 /// To check, whether a wrap was done, check if the return type is `Cow::Borrowed` vs `Cow::Owned`
 /// If a wrap was done, also hoists crate attributes to the top so they keep working
 pub fn maybe_wrap(code: &str, result_handling: ResultHandling) -> Cow<'_, str> {
-	maybe_wrapped(code, result_handling, false)
+	maybe_wrapped(code, result_handling, false, false)
 }
 
-pub fn maybe_wrapped(code: &str, result_handling: ResultHandling, unsf: bool) -> Cow<'_, str> {
-	use syn::{parse, parse::Parse, parse_str, Attribute, Block, Item, ItemFn, Result, Stmt};
+pub fn maybe_wrapped(
+	code: &str,
+	result_handling: ResultHandling,
+	unsf: bool,
+	pretty: bool,
+) -> Cow<'_, str> {
+	#[allow(clippy::wildcard_imports)]
+	use syn::{parse::Parse, *};
 
 	// We use syn to check whether there is a main function.
 	struct Inline {}
@@ -235,6 +241,7 @@ pub fn maybe_wrapped(code: &str, result_handling: ResultHandling, unsf: bool) ->
 	let mut after_crate_attrs = match result_handling {
 		ResultHandling::None => "fn main() {\n",
 		ResultHandling::Discard => "fn main() { let _ = {\n",
+		ResultHandling::Print if pretty => "fn main() { println!(\"{:#?}\", {\n",
 		ResultHandling::Print => "fn main() { println!(\"{:?}\", {\n",
 	}
 	.to_owned();